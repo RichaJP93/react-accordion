64ca0105d14bdcbbc706260ae1336361d9a8bf7a
Joseph Richardson <richardsonjp93@gmail.com>
2021-03-29 11:32:43 +0100

    Question component rendered, info hidden by default, button clicked to show info

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const Question = ({title, info}) => {
+  const [showInfo, setShowInfo] = useState(false)
   return (
-    <div>
-      <h6>{title}</h6>
-      <p>{info}</p>
+    <div className="question">
+      <header>
+        <h6>{title}</h6>
+        <button className="btn" onClick={() => {setShowInfo(!showInfo)}}>
+          {showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
     </div>
   )
 }
